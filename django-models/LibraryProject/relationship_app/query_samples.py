05acb2b278b58a5c3982ac492bc747f3b7275d25
miracleadakole <109351488+miracleadakole@users.noreply.github.com>
2025-11-23 22:32:00 +0100

    Final commit of relationship-model and query_sample

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -14,5 +14,5 @@ def books_in_library(library_name):
 
 # 3. Retrieve the librarian for a library
 def librarian_for_library(library_name):
-    library = Librarian.objects.get(name=library_name)
-    return library.librarian
+    library = Library.objects.get(name=library_name)
+    return librarian.objects.get(library=library)
