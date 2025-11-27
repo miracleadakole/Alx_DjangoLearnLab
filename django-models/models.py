32695c09735dd6dece648fa9d318d8a4c344518c
miracleadakole <109351488+miracleadakole@users.noreply.github.com>
2025-11-27 15:08:13 +0100

    Commit role-based control

--- django-models/models.py
+++ django-models/models.py
@@ -10,7 +10,7 @@ class UserProfile(models.Model):
         ('Member', 'Member'),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default= 'Member')
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
