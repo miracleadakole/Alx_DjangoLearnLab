522ae0dd9159fc2f5714de20420631b220fea2ab
miracleadakole <109351488+miracleadakole@users.noreply.github.com>
2025-11-27 15:36:55 +0100

    Add the implementation of role-based control

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,31 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 from django.shortcuts import redirect
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
+
+# Helper functions to check roles
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Role-based views
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'django-models/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'django-models/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'django-models/member_view.html')
+
 
 # Function-based view to list all books
 def list_books(request):
