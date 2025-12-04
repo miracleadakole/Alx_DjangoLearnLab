f0e97127129d86620286bfea61b2e20af70baa39
miracleadakole <109351488+miracleadakole@users.noreply.github.com>
2025-12-04 22:52:56 +0100

    Add exampleform to security

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from .models import Book
 
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
