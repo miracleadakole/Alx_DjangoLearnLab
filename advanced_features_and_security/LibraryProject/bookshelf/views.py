da738c1cd267e07229731fab68283eaee72a8a9e
miracleadakole <109351488+miracleadakole@users.noreply.github.com>
2025-12-04 22:56:59 +0100

    Commit exampleform to views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 def index(request):
     return HttpResponse("Welcome to the Bookshelf App!")
@@ -47,3 +48,14 @@ def search_books(request):
     query = request.GET.get('q', '')
     books = Book.objects.filter(title__icontains=query)
     return render(request, "bookshelf/book_list.html", {"books": books, "query": query})
+
+def form_example_view(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            messages.success(request, "Form submitted successfully!")
+            return redirect("form_example")
+    else:
+        form = ExampleForm()
+
+    return render(request, "bookshelf/form_example.html", {"form": form})
